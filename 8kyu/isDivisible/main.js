2d921e200aadccd69978a84fc6b522cea26de399
khrystynarolska <kristinarolskaya2017@gmail.com>
2021-11-05 13:38:52 +0200

    code improvement

--- 8kyu/isDivisible/main.js
+++ 8kyu/isDivisible/main.js
@@ -1,10 +1,5 @@
 // Створіть функцію, яка перевіряє, чи число n ділиться на два числа x І y. Усі вхідні дані є додатними, відмінними від нуля цифрами.
 
-function isDivisible(n, x, y) {
-  if (n % x == 0 || n % y == 0) {
-    return true 
-  }
-  
-  return false
-}
-console.log(isDivisible(100, 5, 3))
+const isDivisible = (n, x, y) => n % x == 0 && n % y == 0;
+
+console.log(isDivisible(100, 5, 2))
