e82743a967a0361b3c1125261543bb81766043f9
khrystynarolska <kristinarolskaya2017@gmail.com>
2021-11-08 17:10:33 +0200

    code improvement

--- 8kyu/ReversedStrings/main.js
+++ 8kyu/ReversedStrings/main.js
@@ -2,10 +2,19 @@
 
 function solution(str) {
   let o = '';
-  
+
   for (let i = str.length - 1; i >= 0; i--)
     o += str[i];
   return o;
 }
 
 console.log(solution('world'))
+
+
+///
+
+const solution = str => str.split('').reverse().join('');
+
+///
+
+const solution = str => [...str].reverse().join('')
