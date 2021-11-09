84c28b71548b08a92c36b1f4ed0eb01233b187c0
khrystynarolska <kristinarolskaya2017@gmail.com>
2021-11-09 17:32:26 +0200

    task: Convert a Number to a String

---
+++ 8kyu/ConvertNumberToString/main.js
@@ -0,0 +1,8 @@
+// We need a function that can transform a number into a string.
+
+const numberToString = (num) => {
+  return String(num)
+}
+
+console.log(numberToString(12))
+console.log(typeof numberToString(12))
