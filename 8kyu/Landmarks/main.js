23a7b4783117a026d676013efdf34e869585ea7d
khrystynarolska <kristinarolskaya2017@gmail.com>
2021-12-17 16:26:26 +0200

    landmarks

---
+++ 8kyu/Landmarks/main.js
@@ -0,0 +1,18 @@
+const landmarks = []
+landmarks.push('My house');
+landmarks.push('Front path');
+landmarks.push('Flickering streetlamp');
+landmarks.push('Leaky fire hydrant');
+landmarks.push('Fire station');
+landmarks.push('My old school');
+landmarks.push('My friend house');
+
+landmarks.reverse()
+
+landmarks.shift();
+landmarks.shift();
+landmarks.shift();
+landmarks.shift();
+landmarks.shift();
+landmarks.shift();
+landmarks.shift();
