bc6766ba045237852cd7dca1bde090d2cdf5982b
khrystynarolska <kristinarolskaya2017@gmail.com>
2021-12-22 16:00:41 +0200

    cycle

--- 8kyu/ScoresAndCrazyObject/main.js
+++ 8kyu/ScoresAndCrazyObject/main.js
@@ -13,6 +13,7 @@ scores.Jimmy += 2;
 scores.Katy -=2;
 // console.log(scores)
 
+
 // 2
 let myCrazyObject = {
   name: 'A ridiculous object',
@@ -20,4 +21,48 @@ let myCrazyObject = {
   randomAnimal: 'Banana Shark'
 }
 
-console.log(myCrazyObject.someArray[2].number)
+// console.log(myCrazyObject.someArray[2].number)
+
+
+// 3
+// const user = '';
+// const myName = 'Khrysytyna';
+
+// if (user === myName) {
+//   console.log('Hello me!') 
+// } else {
+//   console.log('Hello stranger!')
+// }
+
+
+// 4
+// let sheepCounted = 0;
+
+// while (sheepCounted <= 10) {
+//   console.log('I have counted ' + sheepCounted + ' sheep!')
+//   sheepCounted++;
+// }
+// console.log('Zzzzzzzz');
+
+
+// 5
+// let animals = ['Lion', 'Flamingo', 'Polar Bear', 'Boa Constrictor'];
+
+// for (let i = 0; i < animals.length; i++) {
+//   console.log('This zoo contains a ' + animals[i] + '.')
+// }
+
+
+// 6
+// for (let x = 2; x < 10000; x = x * 2) {
+//   console.log(x)
+// }
+
+
+//7
+let x = 2;
+
+while (x < 10000) {
+  console.log(x)
+  x = x * 2
+}
