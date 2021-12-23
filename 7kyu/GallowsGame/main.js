195f22c75114b89707aeec4c863301e2cd7d4171
khrystynarolska <kristinarolskaya2017@gmail.com>
2021-12-23 15:57:22 +0200

    game Gallows

---
+++ 7kyu/GallowsGame/main.js
@@ -0,0 +1,48 @@
+// Створюємо масив із слів
+
+let words = [
+  'javascript',
+  'monkey',
+  'amazing',
+  'pancake'
+]
+
+// Обираємо випадкове слово
+let word = words[Math.floor(Math.random() * words.length)];
+
+// Встановлюємо масив відповідей
+let answerArray = [];
+for (let i = 0; i < word.length; i++) {
+  answerArray[i] = '_';
+}
+
+let remainingLetters = word.length;
+
+// Ігравий цикл
+while (remainingLetters > 0) {
+  // Показує гравцю його прогрес
+  alert(answerArray.join(' '));
+
+  // Отримаємо варіанти відповідк=ей від гравця
+  let guess = prompt('Guess a letter, or click Canncel to stop playing.');
+
+  if (guess === null) {
+    //Виходимо з ігрвого циклу
+    break;
+  } else if (guess.length !== 1) {
+    alert('Please enter a single letter.')
+  } else {
+    // Оновлюємо стан гри за допомогою змінного параметра guess
+    for (let j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+        answerArray[j] = guess;
+        remainingLetters--;
+      }
+    }
+  }
+  // Кінець ігрового циклу
+}
+
+// Показуємо відповідь та вітаємо гравця
+alert(answerArray.join(' '));
+alert('Good job! The answer was ' + word);
