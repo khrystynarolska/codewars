f8cfa0be4c59f65133ec99ee7b53e952ae5b7d98
khrystynarolska <kristinarolskaya2017@gmail.com>
2022-03-23 18:27:58 +0200

    Guess the number

---
+++ 9kyu/GuessTheNumber/guessTheNumber.js
@@ -0,0 +1,19 @@
+function randomNumb() {
+  let num = Math.floor(Math.random() * 100);
+
+  for (let i = 1; i <= 10; i++) {
+    let result = +prompt(`Спроба №${i}. Ведіть число: `)
+
+    if (result == num) {
+      return alert(`Ви вгадали число. Спроб: ${i}. Число: ${num}`)
+    } else if (result > num) {
+      alert(`Ваше число ${result} більше загаданого. Спроб: ${i}`)
+    } else if (result < num) {
+      alert(`Ваше число ${result} меньше загаданого. Спроб: ${i}`)
+    } else {
+      return alert(`Ви не вгадали число. Число: ${num}`)
+    }
+  }
+}
+
+randomNumb()
